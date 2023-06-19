18791f0cd8ca8d4a0fab8e6e34f3987ac8c977d5
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2023-06-19 11:23:11 +0900

    Version1.1 StartPage
    간단한 로그인 페이지 구성을 해봄.
    추가로 필요할 것들
    1. 괜찮은 소개 문구
    2. 사진 추가
    3. 사이트 내에서 추천 수 (사진에 하트 누를 수 있게 하기)에 따라 사진 후보가 바뀔 수 있다면 좋을 듯

---
+++ StartPage/start.js
@@ -0,0 +1,10 @@
+const pic = document.getElementById("startingpageImage");
+let num = 0
+const piclst = ["괭이갈매기.jpg","긴꼬리딱새.jpg","꼬까참새.jpg","흰눈썹황금새.jpg"];
+function changePic(){
+    console.log("changed!");
+    pic.src = piclst[num];
+    num++;
+    if (num==4){num = 0;}
+}
+setInterval(changePic,1*1000*10)
